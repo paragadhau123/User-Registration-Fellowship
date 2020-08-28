3b6495e3847d8308286b71bd27ce6b5d406b2263
Parag Adhau <adhauparag64@gmail.com>
2020-08-29 01:50:26 +0530

    Added :Functionality of password validation by rule 4

---
+++ TestUserValidator.java
@@ -0,0 +1,76 @@
+import com.bridgelabz.userregistration.UserValidator;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestUserValidator {
+    @Test
+    public void givenFirstName_WhenProper_ShouldReturn_True() {
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validateFirstName("Parag");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenFirstName_WhenNotProper_ShouldReturn_False() {
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validateFirstName("Sh");
+        Assert.assertEquals(false, result);
+    }
+    @Test
+    public void givenLastName_WhenProper_ShouldReturn_True() {
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validateLastName("Adhau");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenLastName_WhenNotProper_ShouldReturn_False() {
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validateLastName("adg");
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void givenEmail_WhenProper_ShouldReturn_True() {
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validateEmail("abc+111@yahoo.com");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenEmail_WhenNotProper_ShouldReturn_False() {
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validateEmail("abc()*@gmail.com");
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void givenPhone_WhenProper_ShouldReturn_True() {
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validatePhone("91 9604445258");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenPhone_WhenNotProper_ShouldReturn_False() {
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validatePhone("8105215414");
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void givenPassword_WhenProper_ShouldReturn_True() {
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validatePassword("abcdfdA@#fg0");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenPassword_WhenNotProper_ShouldReturn_False() {
+        UserValidator userValidator = new UserValidator();
+        boolean result = userValidator.validatePassword("adasdas0daS");
+        Assert.assertEquals(false, result);
+    }
+}
+
+
