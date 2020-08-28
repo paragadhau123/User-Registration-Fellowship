810e45a8865e70d6ff1378e1da28e1ea5997b90e
Parag Adhau <adhauparag64@gmail.com>
2020-08-29 00:42:43 +0530

    Added :Functionality to validate user email address

--- UserValidator.java
+++ UserValidator.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 public class UserValidator {
 
     private static final String NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
+    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9.+_-]+[@][a-zA-Z0-9]+[.]co(m|.in)$";
 
     public boolean validateFirstName(String firstName) {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
@@ -14,5 +15,10 @@ public class UserValidator {
     public boolean validateLastName(String lastName) {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
         return pattern.matcher(lastName).matches();
+    }
+
+    public boolean validateEmail(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        return pattern.matcher(email).matches();
     }
 }
