7006102fb10d470802ce75bb2b7bf7381f578d93
Parag Adhau <adhauparag64@gmail.com>
2020-08-29 01:14:14 +0530

    Added :Functionality to validate password by rule 1

--- UserValidator.java
+++ UserValidator.java
@@ -7,6 +7,7 @@ public class UserValidator {
     private static final String NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
     private static final String EMAIL_PATTERN = "^[a-zA-Z0-9.+_-]+[@][a-zA-Z0-9]+[.]co(m|.in)$";
     private static final String PHONE_PATTERN = "^[1-9][0-9]+[ ]{0,1}+[1-9][0-9]{9}$";
+    private static final String PASSWORD_PATTERN = "[a-zA-Z0-9]{8,40}";
 
     public boolean validateFirstName(String firstName) {
         Pattern pattern = Pattern.compile(NAME_PATTERN);
@@ -22,8 +23,14 @@ public class UserValidator {
         Pattern pattern = Pattern.compile(EMAIL_PATTERN);
         return pattern.matcher(email).matches();
     }
+
     public boolean validatePhone(String phone) {
         Pattern pattern = Pattern.compile(PHONE_PATTERN);
         return pattern.matcher(phone).matches();
+    }
+
+    public boolean validatePassword(String password) {
+        Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+        return pattern.matcher(password).matches();
     }
 }
